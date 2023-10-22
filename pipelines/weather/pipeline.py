b635db1267a11d925ed6475462c4f1d58f1509ed
Casey Whorton <27929027+caseywhorton@users.noreply.github.com>
2023-10-21 21:01:46 -0400

    Update pipeline.py

--- pipelines/weather/pipeline.py
+++ pipelines/weather/pipeline.py
@@ -540,7 +540,7 @@ def get_pipeline(
         destination = f"{output_path}evaluation",
         output_name = "evaluation")
     ]
-    code=BASE_DIR + "evaluate.py"
+    code=BASE_DIR + "/evaluate.py"
 
     evaluation_report = PropertyFile(
     name="WeatherForecastEvaluationReport",
